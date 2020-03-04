0ac824361c1bdb3e2e571f5df5f042c7d405a9bb
Angad Dubey <angad_dubey@hotmail.com>
2020-03-04 18:57:06 -0500

    Dropdown fxn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Princy",
-      animal: "Dog",
-      breed: "Shepherd"
-    }),
-    React.createElement(Pet, { name: "Simba", animal: "Dog", breed: "Lab" }),
-    React.createElement(Pet, { name: "Athena", animal: "Cat", breed: "Doink" })
-  ]);
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>  
+        </header>
+        <Router>
+          <SearchParams path="/" />  
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/Details.js
@@ -0,0 +1,48 @@
+import React from "react";
+import pet from "@frontendmasters/pet";
+
+class Details extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const { id } = this.props;
+    pet.animal(id)
+    .then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    if(this.state.loading) {
+      return <h1>loading ...</h1>;
+    }
+    const { animal, breed, location, description, name} = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Details;
